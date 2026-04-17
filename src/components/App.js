5cdce85b14d39df1bb3c245b6c036ef58026c334
imhsart <nitinrana2519@gmail.com>
2026-04-17 17:36:46 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
-
+import ChildComponent from './TodoList'
 const App = () => {
+  const [todos, setTodos] = useState([
+    {todo:'Learn React', status:false}, 
+    {todo:'Build a React app', status:false},
+    {todo:'Deploy the React app', status:false}
+  ])
+
+  const handleComplete = (idx) => {
+    setTodos(todos.map((val,index) => index === idx ? {...val, status:true} : val))
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+        <ChildComponent todos={todos} handleComplete={handleComplete} />
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ChildComponent = ({todos, handleComplete}) => {
+
+  return (
+    <div className='child'>
+      <h2>Child Component</h2>
+      <ul>
+        {
+          todos.map((item,index) => {
+            return <li key={`item-${index}`} >{item.todo} {!item.status && <button onClick={() => handleComplete(index)}>Complete</button>}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default ChildComponent
