c5879b46af12078a0bfa9b5c543685af5ace0090
imhsart <nitinrana2519@gmail.com>
2026-04-17 20:16:17 +0530

    Assignment attempt 11

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import './../styles/App.css';
 import ChildComponent from './TodoList'
 const App = () => {
   const [todos, setTodos] = useState([
-    {todo:'Learn React', completed:true}, 
-    {todo:'Build a React app', completed:false},
-    {todo:'Deploy the React app', completed:false}
+    {id:1,todo:'Learn React', completed:false}, 
+    {id:2,todo:'Build a React app', completed:false},
+    {id:3,todo:'Deploy the React app', completed:false}
   ])
 
   const handleComplete = (idx) => {
-    setTodos(prev => prev.map((val,index) => index === idx ? {...val, completed:true} : val))
+    setTodos(prev => prev.map(val => val.id === idx ? {...val, completed:true} : val))
   }
 
   return (
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,8 +7,8 @@ const ChildComponent = ({todos, handleComplete}) => {
       <h2>Child Component</h2>
       <ul>
         {
-          todos.map((item,index) => {
-            return <li key={`item-${index}`}>{item.todo}{item.completed ? null : <button onClick={() => handleComplete(index)}>Complete</button>}</li>
+          todos.map(item => {
+            return <li key={`item-${item.id}`}>{item.todo}{item.completed ? null : <button onClick={() => handleComplete(item.id)}>Complete</button>}</li>
           })
         }
       </ul>
