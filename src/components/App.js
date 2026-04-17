79b93a67e39a38552a1af352b69f564a90e37cb5
imhsart <nitinrana2519@gmail.com>
2026-04-17 17:49:43 +0530

    Assignment attempt 2

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import './../styles/App.css';
 import ChildComponent from './TodoList'
 const App = () => {
   const [todos, setTodos] = useState([
-    {todo:'Learn React', status:false}, 
-    {todo:'Build a React app', status:false},
-    {todo:'Deploy the React app', status:false}
+    {todo:'Learn React', completed:false}, 
+    {todo:'Build a React app', completed:false},
+    {todo:'Deploy the React app', completed:false}
   ])
 
   const handleComplete = (idx) => {
-    setTodos(todos.map((val,index) => index === idx ? {...val, status:true} : val))
+    setTodos(todos.map((val,index) => index === idx ? {...val, completed:true} : val))
   }
 
   return (
